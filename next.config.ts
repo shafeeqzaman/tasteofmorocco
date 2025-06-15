35e85047639ed196dc391492da919c9d2fe4cb2a
Zaman <shafeeq.zaman@ataccama.com>
2025-06-14 22:47:25 -0400

    Allow Vercel to build despite type errors (ignoreBuildErrors)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,  // allow type errors to pass build
+  },
+  eslint: {
+    ignoreDuringBuilds: true,  // allow eslint errors to pass build
+  },
 };
 
 export default nextConfig;
