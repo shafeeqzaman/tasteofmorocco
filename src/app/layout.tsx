05ed637fcf377e775340b6f97db1321569f41f98
Zaman <33529203+shafeeqzaman@users.noreply.github.com>
2025-06-18 17:37:22 -0400

    Add vercel's Speed Insigts package to layout.tsx to start collecting performance metrics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@fontsource/merriweather/700.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 import "./globals.css";
